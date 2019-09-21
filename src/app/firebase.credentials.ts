9c8f23e1d5e51ddd35a9c05ec52e543afa3585f3
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2019-09-21 17:02:20 -0300

    Update firebase.credentials.ts

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,9 +1,9 @@
 export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAxb7JjA5_KFKmpcl7dXN8-ACC2zmSIym8",
-  authDomain: "ionicshoppinglist-3c719.firebaseapp.com",
-  databaseURL: "https://ionicshoppinglist-3c719.firebaseio.com",
-  projectId: "ionicshoppinglist-3c719",
-  storageBucket: "ionicshoppinglist-3c719.appspot.com",
-  messagingSenderId: "490448879839",
-  appId: "1:490448879839:web:dd77185cabb63dd330e96a"
+  apiKey: "...",
+  authDomain: "...",
+  databaseURL: "...",
+  projectId: "...",
+  storageBucket: "....",
+  messagingSenderId: "...",
+  appId: "..."
 };
